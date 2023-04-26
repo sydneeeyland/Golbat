2fe6b87c05ba17c2c2ddf6c66268e80384254db8
sydney-singlifeph <sydneydiala@singlife.com>
2023-04-26 08:30:28 +0800

    initialized project, setup linters, testing libraries, prettier

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+// Package
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Application
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
