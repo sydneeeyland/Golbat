69d57cebe5c16a58040f287cbc0191fc23dbd5d4
sydney-singlifeph <sydneydiala@singlife.com>
2023-06-11 07:42:36 +0800

    progress

--- src/Pages/Authentication/SignIn.tsx
+++ src/Pages/Authentication/SignIn.tsx
@@ -1,103 +1,79 @@
-import React, { useState } from 'react';
-
-import LoginIcon from '@mui/icons-material/Login';
-import { Facebook, Google } from '@mui/icons-material';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-
-import {
-  Box,
-  Typography,
-  TextField,
-  Button,
-  FormControl,
-  InputLabel,
-  InputAdornment,
-  IconButton,
-  Input,
-} from '@mui/material';
-
-import { useLoginStyles } from '../../Assets/Styles/LoginStyles';
+import { Grid, Card, Typography, Stack, Box, Button } from '@mui/material';
+import { Form } from 'react-bootstrap';
 
 function SignIn() {
-  // TODO: Refactor, Code Cleanup
-  const classes = useLoginStyles();
-
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
-
   return (
-    <Box className={classes.wrapper}>
-      <Box className={classes.container}>
-        <Typography variant="h3" gutterBottom>
-          Login
-        </Typography>
-        <Box className={classes.inputContainer}>
-          <TextField
-            sx={{ mb: 2, width: '100%' }}
-            id="standard-basic"
-            label="Username"
-            variant="standard"
-          />
-        </Box>
-        <Box className={classes.inputContainer}>
-          <FormControl
-            sx={{ mb: 2, width: '100%' }}
-            variant="standard"
-            size="small"
-          >
-            <InputLabel htmlFor="standard-adornment-password">
-              Password
-            </InputLabel>
-            <Input
-              id="standard-adornment-password"
-              type={showPassword ? 'text' : 'password'}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-        </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-          <Box sx={{ flex: 1 }}>
-            <input type="checkbox" defaultChecked name="remember" /> Remember Me
+    <Grid
+      container
+      xs={12}
+      justifyContent="center"
+      alignItems="center"
+      flexBasis={1}
+      height="100vh"
+      sx={{ backgroundColor: 'whitesmoke' }}
+    >
+      <Grid item>
+        <Card
+          variant="elevation"
+          sx={{
+            backgroundColor: 'white',
+            color: 'black',
+            transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+            margin: '24px',
+            overflow: 'hidden',
+            position: 'relative',
+            border: 'none rgb(230, 235, 241)',
+            borderRadius: '4px',
+            boxShadow: 'rgba(0, 0, 0, 0.08) 0px 1px 4px',
+            width: '400px',
+          }}
+        >
+          <Box sx={{ padding: '32px' }}>
+            <Stack flexDirection="row" display="flex" spacing="4px">
+              <Typography flex={1} variant="h5">
+                Login
+              </Typography>
+              <Typography
+                flex={1}
+                sx={{ fontSize: '0.875rem', textAlign: 'end' }}
+              >
+                Don&apos;t have an account?
+              </Typography>
+            </Stack>
+            <Stack
+              flexDirection="column"
+              spacing="4px"
+              sx={{ marginTop: '24px' }}
+            >
+              <Typography>Employee ID</Typography>
+              <Form.Control type="text" />
+            </Stack>
+            <Stack
+              flexDirection="column"
+              spacing="4px"
+              sx={{ marginTop: '24px' }}
+            >
+              <Typography>Password</Typography>
+              <Form.Control type="password" />
+            </Stack>
+            <Stack flexDirection="row" spacing="3px" sx={{ marginTop: '24px' }}>
+              <Stack spacing="3px" gap="6px" flexDirection="row">
+                <input type="checkbox" />
+                <Typography>Keep me Signed in</Typography>
+              </Stack>
+              <Typography flex={1} textAlign="end">
+                Forgot Password?
+              </Typography>
+            </Stack>
+            <Box sx={{ marginTop: '24px' }}>
+              <Button variant="csi-primary" sx={{ padding: '10px' }} fullWidth>
+                Login
+              </Button>
+            </Box>
           </Box>
-          <Box sx={{ flex: 1, textAlign: 'end' }}>Forgot password</Box>
-        </Box>
-        <Button fullWidth variant="contained" startIcon={<LoginIcon />}>
-          Login
-        </Button>
-        <Box className="Sign">
-          <Typography component="span">Not yet a member? sign up</Typography>
-        </Box>
-        <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>
-          Or Login with
-        </Typography>
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Box>
-            <Facebook color="primary" fontSize="large" />
-          </Box>
-          <Box>
-            <Google color="primary" fontSize="large" />
-          </Box>
-        </Box>
-      </Box>
-    </Box>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
--- src/Pages/Error/Unauthorized.tsx
+++ src/Pages/Error/Unauthorized.tsx
@@ -1,5 +1,31 @@
+import { Grid, NoSsr, Box } from '@mui/material';
+
+import UnauthorizedHero from '../../Assets/Images/401-Unauthorized.svg';
+
 function Unauthorized() {
-  return <div>Unauthorized</div>;
+  return (
+    <NoSsr defer>
+      <Grid
+        container
+        spacing={4}
+        justifyContent="center"
+        alignItems="center"
+        minHeight="85vh"
+        direction="column"
+      >
+        <Grid item xs={12}>
+          <Box minWidth="480px">
+            <img
+              src={UnauthorizedHero}
+              alt="maintenance"
+              width="100%"
+              height="auto"
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    </NoSsr>
+  );
 }
 
 export default Unauthorized;
